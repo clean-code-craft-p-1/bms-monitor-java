d05f9053609cefd568e780a28fc2bed8fd1b1c25
Sudeep Prasad <sudeep.rp@gmail.com>
2025-03-30 20:12:52 +0530

    bio vitals

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -1,15 +1,34 @@
 package vitals;
 
-public class VitalsChecker {
-  static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-    if (temperature < 0 || temperature > 45) {
-      System.out.println("Temperature is out of range!");
+
+public abstract class VitalsChecker {
+  static boolean vitalsOk(float temperature, float pulseRate, float spo2) throws InterruptedException {
+    if (temperature > 102 || temperature < 95) {
+      System.out.println("Temperature is critical!");
+      for (int i = 0; i < 6; i++) {
+        System.out.print("\r* ");
+        Thread.sleep(1000);
+        System.out.print("\r *");
+        Thread.sleep(1000);
+      }
       return false;
-    } else if (soc < 20 || soc > 80) {
-      System.out.println("State of Charge is out of range!");
+    } else if (pulseRate < 60 || pulseRate > 100) {
+      System.out.println("Pulse Rate is out of range!");
+      for (int i = 0; i < 6; i++) {
+        System.out.print("\r* ");
+        Thread.sleep(1000);
+        System.out.print("\r *");
+        Thread.sleep(1000);
+      }
       return false;
-    } else if (chargeRate > 0.8) {
-      System.out.println("Charge Rate is out of range!");
+    } else if (spo2 < 90) {
+      System.out.println("Oxygen Saturation out of range!");
+      for (int i = 0; i < 6; i++) {
+        System.out.print("\r* ");
+        Thread.sleep(1000);
+        System.out.print("\r *");
+        Thread.sleep(1000);
+      }
       return false;
     }
     return true;
--- src/test/java/vitals/VitalsCheckerTest.java
+++ src/test/java/vitals/VitalsCheckerTest.java
@@ -2,30 +2,17 @@ package vitals;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import vitals.VitalsChecker;
 
 
 public class VitalsCheckerTest {
 
   @Test
-  public void testBatteryIsOkWithinRange() {
-    assertTrue(VitalsChecker.batteryIsOk(25, 50, 0.5f));
-  }
-
-  @Test
-  public void testBatteryIsOkTemperatureOutOfRange() {
-    assertFalse(VitalsChecker.batteryIsOk(-1, 50, 0.5f));
-    assertFalse(VitalsChecker.batteryIsOk(46, 50, 0.5f));
-  }
-
-  @Test
-  public void testBatteryIsOkSocOutOfRange() {
-    assertFalse(VitalsChecker.batteryIsOk(25, 19, 0.5f));
-    assertFalse(VitalsChecker.batteryIsOk(25, 81, 0.5f));
-  }
-
-  @Test
-  public void testBatteryIsOkChargeRateOutOfRange() {
-    assertFalse(VitalsChecker.batteryIsOk(25, 50, 0.9f));
+  public void testNotOkWhenAnyVitalIsOffRange() throws InterruptedException {
+    assertFalse(VitalsChecker.vitalsOk(99f, 102, 70));
+    assertTrue(VitalsChecker.vitalsOk(98.1f, 70, 98));
   }
 }
